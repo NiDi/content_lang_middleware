3aa8bae3f813e614f68e9ac32237e1618626d476
Nicolas Dillmann <nicolas.dillmann@zweitag.de>
2011-04-20 17:13:55 +0200

    remove ::

--- lib/content_lang_middleware.rb
+++ lib/content_lang_middleware.rb
@@ -23,7 +23,7 @@ module ContentLangMiddleware
 
     def call(env)
       status, headers, body = @app.call(env)
-      headers = ::Utils::HeaderHash.new(headers)
+      headers = Utils::HeaderHash.new(headers)
       headers['Content-Language'] ||= "de" 
       [status, headers, body]
     end
